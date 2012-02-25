00b57a0f05f8cdfaeb44a78089e631f48cae330a
Michael Lawson <mdlawson@gmx.com>
2012-02-25 15:26:10 +0000

    testing some models out

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,22 @@
-/* Author:
+$(function(){
+	_.templateSettings = {
+		interpolate : /\{\{(.+?)\}\}/g
+	};
+	window.Album = Backbone.Model.extend({});
+	window.Albums = Backbone.Collection.extend({
+		model: Album
+	});
+	window.AlbumView = Backbone.View.extend({
+		tagName: "li",
+		className: "album",
+		template: _.template($('#album-template').html()),
+		render: function() {
+			$(this.el).html(this.template(this.model.toJSON()))
+			return this;
+		},
 
-*/
-
-
+	})
+})
 
 
 
